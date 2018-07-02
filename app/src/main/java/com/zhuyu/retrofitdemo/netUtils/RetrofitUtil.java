0012be632401774519869c7de9935bd4e6af2adc
zhuyu <757693435@qq.com>
2018-07-02 18:06:25 +0800

    get请求

---
+++ app/src/main/java/com/zhuyu/retrofitdemo/netUtils/RetrofitUtil.java
@@ -0,0 +1,42 @@
+package com.zhuyu.retrofitdemo.netUtils;
+
+import com.zhuyu.retrofitdemo.Api;
+import com.zhuyu.retrofitdemo.Constant;
+
+import retrofit2.Retrofit;
+
+/**
+ * Name: RetrofitUtil
+ * Author: zhu_yu
+ * Email:
+ * Comment: //TODO
+ * Date: 2018-07-02 17:29
+ */
+public class RetrofitUtil {
+
+    private static RetrofitUtil mRetrofitUtil;
+    private  Retrofit mRetrofit;
+    public static  RetrofitUtil getInstanse() {
+        if (mRetrofitUtil == null) {
+            mRetrofitUtil = new RetrofitUtil();
+        }
+        return mRetrofitUtil;
+    }
+
+
+
+    private RetrofitUtil() {
+        initRetrofit();
+    }
+
+    private void initRetrofit() {
+
+        mRetrofit = new Retrofit.Builder().baseUrl(Constant.BASE_URL).build();
+
+    }
+
+    public Api createApi(){
+       return mRetrofit.create(Api.class);
+    }
+
+}
